a8874ee1e20c21aabff17c77032965a52a66f9a3
GustavoW <guvieirawalter@gmail.com>
2026-03-11 20:55:55 -0300

    Adicionado import asynccontextmanager no main.py

--- src/main.py
+++ src/main.py
@@ -2,13 +2,24 @@ from fastapi import FastAPI
 from src.settings import HOST, PORT, RELOAD
 import uvicorn
 
-from src.routers import FuncionarioRouter, ClienteRouter, ProdutoRouter
+from routers import FuncionarioRouter, ClienteRouter, ProdutoRouter
 
-app = FastAPI()
+from infra import database
+from contextlib import asynccontextmanager
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("API has started")
+
+    await database.cria_tabelas()
+    yield
+
+    print("API is shutting down")
+
+app = FastAPI(lifespan = lifespan)
 
 # Gustavo Vieira Walter
 @app.get("/", tags=["Root"], status_code=200)
-def root():
+async def root():
     return {"detail":"API Pastelaria", "Swagger UI": "http://127.0.0.1:8000/docs", "ReDoc": "http://127.0.0.1:8000/redoc" }
 
 app.include_router(FuncionarioRouter.router)
